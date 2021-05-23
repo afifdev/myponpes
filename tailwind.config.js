e4498f754e3ae60fadc4bc19549436678a6a79fd
afifvdin <afifudin.fdn@gmail.com>
2021-05-23 09:48:31 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  darkMode: "class",
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    options: {
+      keyframes: true,
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
