eb52bb7b60e76eb1ef1d4ef0e59e2c0c5c21cbd8
afifvdin <afifudin.fdn@gmail.com>
2021-05-27 15:02:51 +0700

    add profile component

---
+++ src/components/moleculs/Profile.jsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AdminProfilePic } from "../../assets";
+import { UserContext } from "../../context/UserContext";
+
+const Profile = ({ show }) => {
+  const { user } = useContext(UserContext);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [img, setImg] = useState("");
+
+  const getBasicProfile = async () => {
+    if (user.level > 1) {
+      setName("Admin");
+      setUsername("@admin");
+      return true;
+    }
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+    const { data } = await axios.get(`user?needing=profile`, config);
+    setUsername(data.data.username);
+    setName(data.data.name);
+    setImg(data.data.image);
+  };
+
+  useEffect(() => {
+    getBasicProfile();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div
+      className={`${
+        show ? "flex" : "hidden"
+      } lg:flex flex-col items-center py-10`}
+    >
+      <img
+        className="object-cover w-24 h-24 rounded-full ring ring-offset-4 ring-red-500"
+        src={
+          user.level === 2 ? (
+            <AdminProfilePic />
+          ) : (
+            `http://localhost:4000/${img}`
+          )
+        }
+        alt="profilepic"
+      />
+      <p className="text-xl font-medium pt-4">{name}</p>
+      <p className="font-medium text-gray-400 leading-3 pb-2">{username}</p>
+    </div>
+  );
+};
+
+export default Profile;
