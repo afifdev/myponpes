1d37fea9e64bca6cd74a173715421b6b4cf2b5dc
afifvdin <afifudin.fdn@gmail.com>
2021-05-27 15:02:15 +0700

    code splitting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,128 +1,27 @@
-import { useState } from "react";
-import { BgLogin } from "../assets";
+import { useContext, useState } from "react";
+import { useHistory } from "react-router";
+import { UserContext } from "../context/UserContext";
+import { Header, Profile, AdminNav, SantriNav } from "./moleculs";
 
-const Navbar = ({ profilePicUrl, level }) => {
+const Navbar = () => {
   const [show, setShow] = useState(false);
+  const { user } = useContext(UserContext);
+  const history = useHistory();
   const showNav = () => {
     setShow(!show);
+  };
+  const handleLogout = () => {
+    localStorage.removeItem("ponpestoken");
+    return history.push("/");
   };
   return (
     <div className="py-9 px-6 lg:py-0 lg:px-0 lg:pt-6 lg:pl-6 mt-2">
       <div className="fixed left-0 right-0 top-0 p-6 lg:p-0 lg:sticky lg:top-8 bg-gray-100">
-        <div className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-8 w-8 px-1 pb-0.5 bg-gradient-to-r from-red-500 to-yellow-500 rounded-md"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-            />
-          </svg>
-          <p className="pl-4 text-2xl font-medium flex-grow">My Ponpes</p>
-          <button
-            onClick={showNav}
-            className="lg:hidden py-1 px-2 rounded-md bg-gradient-to-r from-red-500 to-yellow-500 text-white"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
+        <Header action={showNav} />
         <div className="w-80"></div>
-        <div
-          className={`${
-            show ? "flex" : "hidden"
-          } lg:flex flex-col items-center py-10`}
-        >
-          <img
-            className="object-cover w-24 h-24 rounded-full ring ring-offset-4 ring-red-500"
-            src={BgLogin}
-            alt=""
-          />
-          <p className="text-xl font-medium pt-4">M Afifudin Abdullah</p>
-          <p className="font-medium text-gray-400 leading-3 pb-2">@afifudin</p>
-        </div>
+        <Profile show={show} />
         <div className={`${show ? "block" : "hidden"} lg:block`}>
-          <a href="/" className="flex items-center my-6 text-red-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-              />
-            </svg>
-            <p className="cursor-pointer flex-grow border-r-2 border-red-500 pt-1 pl-2">
-              Transactions
-            </p>
-          </a>
-          <a href="/" className="flex items-center my-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z" />
-              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              />
-            </svg>
-            <p className="cursor-pointer flex-grow border-r-2 pt-1 pl-2">
-              Santri
-            </p>
-          </a>
-          <a href="/" className="flex items center my-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
-            <p className="cursor-pointer flex-grow border-r-2 pl-2">Settings</p>
-          </a>
+          {user.level > 1 ? <AdminNav /> : <SantriNav />}
           <a href="/" className="flex items-center my-12">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -138,7 +37,11 @@ const Navbar = ({ profilePicUrl, level }) => {
                 d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
               />
             </svg>
-            <p className="cursor-pointer flex-grow border-r-2 pt-1 pl-2">
+            <p
+              onClick={handleLogout}
+              href="/logout"
+              className="cursor-pointer flex-grow border-r-2 pt-1 pl-2"
+            >
               Logout
             </p>
           </a>
