23f2ae1d5141588a2ad3c6d69f36c925c32af877
afifvdin <afifudin.fdn@gmail.com>
2021-05-27 22:29:35 +0700

    basic home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,141 @@
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div className="max-w-8xl m-auto">
+      <div className="flex flex-col lg:flex-row bg-gray-100 lg:bg-transparent min-h-screen">
+        <Navbar />
+        <div className="flex-grow pt-7 bg-white px-6">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="bg-gray-100 rounded-lg p-8 md:col-span-2 lg:col-span-3">
+              <input type="text" name="search" className="w-full" />
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col bg-gray-100 rounded-lg p-8">
+              <p className="text-red-500 font-medium text-xl">
+                Pembayaran Registrasi
+              </p>
+              <p className="text-2xl font-medium pb-4">Due 02 April 2002</p>
+              <div>
+                <button className="px-2 py-1 rounded-lg font-medium text-white bg-gradient-to-r from-red-500 to-yellow-500">
+                  See Detail
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
