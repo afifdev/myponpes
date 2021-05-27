771a3ae601481d76aea7c8a8475555bd670e3dc9
afifvdin <afifudin.fdn@gmail.com>
2021-05-27 15:02:37 +0700

    code splitting to moleculs

---
+++ src/components/moleculs/index.jsx
@@ -0,0 +1,5 @@
+import Header from "./Header";
+import Profile from "./Profile";
+import AdminNav from "./AdminNav";
+import SantriNav from "./SantriNav";
+export { Header, Profile, AdminNav, SantriNav };
