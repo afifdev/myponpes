d54ca387cf192f72985537a2eadf83ab25352b72
afifvdin <afifudin.fdn@gmail.com>
2021-05-28 16:12:18 +0700

    remove import object for better performance

--- src/components/moleculs/index.jsx
+++
@@ -1,5 +0,0 @@
-import Header from "./Header";
-import Profile from "./Profile";
-import AdminNav from "./AdminNav";
-import SantriNav from "./SantriNav";
-export { Header, Profile, AdminNav, SantriNav };
