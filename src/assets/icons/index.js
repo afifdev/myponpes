554c166c4e4777c5d77069c43da090d2d2d8088a
afifvdin <afifudin.fdn@gmail.com>
2021-05-28 16:27:26 +0700

    add exporter, because code splitting only in pages

---
+++ src/assets/icons/index.js
@@ -0,0 +1,21 @@
+import AnalyticsSVG from "./AnalyticsSVG";
+import BrandSVG from "./BrandSVG";
+import GallerySVG from "./GallerySVG";
+import HamburgerSVG from "./HamburgerSVG";
+import HomeSVG from "./HomeSVG";
+import LogoutSVG from "./LogoutSVG";
+import SantriSVG from "./SantriSVG";
+import SettingsSVG from "./SettingsSVG";
+import TransactionsSVG from "./TransactionsSVG";
+
+export {
+  AnalyticsSVG,
+  BrandSVG,
+  GallerySVG,
+  HamburgerSVG,
+  HomeSVG,
+  LogoutSVG,
+  SantriSVG,
+  SettingsSVG,
+  TransactionsSVG,
+};
