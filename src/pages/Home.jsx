2939127a7bfab7eedd959be99dfc1d3030b7051e
afifvdin <afifudin.fdn@gmail.com>
2021-05-29 20:38:10 +0700

    basic nest route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/moleculs/Navbar";
 const AdminHome = lazy(() => import("./admin/AdminHome"));
 const Transactions = lazy(() => import("./admin/Transactions"));
 
@@ -10,10 +10,14 @@ const Home = () => {
       <div className="flex flex-col lg:flex-row min-h-screen">
         <Navbar />
         <div className="flex-grow lg:bg-gray-100 max-w-8xl">
-          <div className="bg-white rounded-tl-3xl pt-20 lg:pt-8 px-6 h-full w-full">
+          <div className="bg-white lg:rounded-tl-3xl pt-20 lg:pt-8 px-6 h-full w-full">
             <Switch>
-              <Route exact path="/" component={AdminHome} />
-              <Route path="/transactions" component={Transactions} />
+              <Suspense fallback={<div>Loading... </div>}>
+                <Route exact path="/" component={AdminHome} />
+              </Suspense>
+              <Suspense fallback={<div>Loading... </div>}>
+                <Route path="/transactions" component={Transactions} />
+              </Suspense>
             </Switch>
           </div>
         </div>
